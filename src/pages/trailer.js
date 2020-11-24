2f70561e65415e9cf13890bfad81e969290f011b
k-tests <43910854+k-tests@users.noreply.github.com>
2020-11-23 23:05:32 -0500

    commit

---
+++ src/pages/trailer.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Footer from "../components/footer"
+import IntroVideo from "../components/introVideo"
+import TopNavFiller from "../components/_topNavFiller"
+import Layout from "../components/layout"
+
+const Trailer = () => {
+  return (
+    <Layout>
+      <TopNavFiller />
+      <IntroVideo />
+    </Layout>
+  )
+}
+
+export default Trailer
