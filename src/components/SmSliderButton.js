1f64baf8c13c30608d1b9ad0b3b4783a94b9b9d2
Mike Vanis <hello@mikevanis.com>
2020-11-26 15:32:41 +0000

    added SmSliderButton

---
+++ src/components/SmSliderButton.js
@@ -0,0 +1,34 @@
+import React, {useRef} from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { Button } from 'react-bootstrap';
+
+export default function SmSliderButton(props) {
+  const containerRef = useRef(null);
+  const buttonRef = useRef(null);
+  const dragControl = useAnimation();
+
+  const handleButtonClick = async (event, info) => {
+    await dragControl.start({
+      x: containerRef.current.offsetWidth - buttonRef.current.offsetWidth,
+    });
+    props.onClick();
+    dragControl.start({
+      x: 0,
+    });
+  };
+
+  return (
+    <div className="slider-button" ref={containerRef}>
+      <motion.div
+        animate={dragControl}
+      >
+        <Button
+          ref={buttonRef}
+          onClick={handleButtonClick}
+        >
+          {props.children}
+        </Button>
+      </motion.div>
+    </div>
+  );
+};
